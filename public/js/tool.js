5f80e8768b858d56b9a8ffa8aa9685d99bb85aa0
Rajesh Kumar <rajesh.bcsf18@iba-suk.edu.pk>
2022-09-01 23:49:16 +0500

    Improved UI

--- public/js/tool.js
+++ public/js/tool.js
@@ -1,31 +1,184 @@
 $(document).ready(() => {
+    var MemeCanvas = new fabric.Canvas('MemeCanvas', {
+        width: 1000,
+        height: 700,
+        backgroundColor: "white"
+    });
+
+    console.log(MemeCanvas)
+
+
     var pixabayApiKey = '29631978-3c4be564185a97a0cedae0ebb';
 
     $('#pixabay-search-btn').click(() => {
         $('#pixabay-image-container').html("<div class='spinner-grow text-primary' role='status'><span class='visually-hidden'>Loading...</span></div>");
         let fieldValue = $('#pixabay-search-field').val();
-        let URL = "https://pixabay.com/api/?key="+pixabayApiKey+"&q="+encodeURIComponent(fieldValue);
-        $.get(URL, function(data, status){
+        let URL = "https://pixabay.com/api/?key=" + pixabayApiKey + "&q=" + encodeURIComponent(fieldValue);
+        $.get(URL, function (data, status) {
             $
-            if(status === "success"){
+            if (status === "success") {
                 let images = "";
                 console.log(data)
                 data.hits.map((item, index) => {
-                    images += `<img src='${item.previewURL}' class='border' alt='${item.tags}'>`;
+                    images += `<img src='${item.previewURL}' class='border net-image' data-originalurl='${item.largeImageURL}' alt='${item.tags}'>`;
                 })
                 $('#pixabay-image-container').html(images);
             }
         });
-    })
+    });
+    $('#pixabay-search-btn').click();
 
-
-    var myCanvas = new fabric.Canvas('MemeCanvas', {
-        width: 700,
-        height: 600,
-        backgroundColor: "white"
+    $('#shapes-cirlce-card').click(() => {
+        let myCircle = new fabric.Circle({
+            radius: 50,
+            fill: 'red',
+            left: 100,
+            top: 100,
+            stroke: 'black',
+            strokeWidth: 5,
+            fill: 'rgba(0,0,0,0)'
+        });
+        MemeCanvas.add(myCircle)
+    });
+    $('#shapes-rectangular-card').click(() => {
+        let myRectangular = new fabric.Rect({
+            left: 100,
+            top: 100,
+            fill: 'red',
+            stroke: 'black',
+            strokeWidth: 5,
+            fill: 'rgba(0,0,0,0)',
+            height: 80,
+            width: 200
+        });
+        MemeCanvas.add(myRectangular)
     });
 
-    console.log(myCanvas)
+    $('#shapes-polygon-card').click(() => {
+        let polygonPoints = [
+            { x: 850, y: 75 },
+            { x: 958, y: 137.5 },
+            { x: 958, y: 262.5 },
+            { x: 850, y: 325 },
+            { x: 742, y: 262.5 },
+            { x: 742, y: 137.5 }];
+
+        let myPolygon = new fabric.Polygon(polygonPoints, {
+            top: 100,
+            left: 100,
+            fill: 'rgba(0,0,0,0)',
+            stroke: 'black',
+            strokeWidth: 5
+        });
+        MemeCanvas.add(myPolygon)
+    });
+
+    $('#shapes-square-card').click(() => {
+        let mySquare = new fabric.Rect({
+            left: 100,
+            top: 100,
+            fill: 'red',
+            stroke: 'black',
+            strokeWidth: 5,
+            fill: 'rgba(0,0,0,0)',
+            height: 100,
+            width: 100
+        });
+        MemeCanvas.add(mySquare)
+
+    });
+
+    $('#shapes-star-card').click(() => {
+        let starPoints = [
+            { x: 350, y: 75 },
+            { x: 380, y: 160 },
+            { x: 470, y: 160 },
+            { x: 400, y: 215 },
+            { x: 423, y: 301 },
+            { x: 350, y: 250 },
+            { x: 277, y: 301 },
+            { x: 303, y: 215 },
+            { x: 231, y: 161 },
+            { x: 321, y: 161 }
+        ];
+        let myStar = new fabric.Polygon(starPoints, {
+            top: 100,
+            left: 100,
+            fill: 'rgba(0,0,0,0)',
+            stroke: 'black',
+            strokeWidth: 5
+        });
+        MemeCanvas.add(myStar)
+
+
+    });
+
+    $('#shapes-triangle-card').click(() => {
+        let myTriangle = new fabric.Triangle({
+            top: 100,
+            left: 100,
+            width: 140,
+            height: 100,
+            fill: 'rgba(0,0,0,0)',
+            stroke: 'black',
+            strokeWidth: 5
+        });
+        MemeCanvas.add(myTriangle);
+    });
+
+    $('#upload-custom-image').change(function (e) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            var image = new Image();
+            image.src = e.target.result;
+            image.onload = function () {
+                var img = new fabric.Image(image);
+                img.set({
+                    left: 100,
+                    top: 100
+                });
+                img.scaleToWidth(200);
+                MemeCanvas.add(img).setActiveObject(img).renderAll();
+            }
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    });
+    $('.local-image').click(function (e) {
+        var image = new Image();
+        image.src = e.target.src;
+        image.onload = function () {
+            var img = new fabric.Image(image);
+            img.set({
+                left: 100,
+                top: 100
+            });
+            img.scaleToWidth(200);
+            MemeCanvas.add(img).setActiveObject(img).renderAll();
+        }
+    });
+
+    $(document).on("click", ".net-image" , function(e) {
+        let imageSrc = $(this).data('originalurl');
+        fabric.Image.fromURL(imageSrc, function (oImg) {
+            
+            oImg.set({
+                left: 100,
+                top: 100
+            });
+            oImg.scale(0.1);
+            MemeCanvas.add(oImg).setActiveObject(oImg).renderAll();;
+        });
+    });
+
+
+
+
+
+
+
+
+
+
 
     $('#typography_add_btn').click(() => {
         var itext = new fabric.IText('Hello World', {
@@ -34,8 +187,7 @@ $(document).ready(() => {
             fontSize: 20,
             fontFamily: "Arial"
         });
-        myCanvas.add(itext);
-        // console.log(itext)
+        MemeCanvas.add(itext);
     });
 });
 
